721ef34c3e00673755a24c6b6b38180d7769857d
Mael Cicciu <mael.cicciu@diiage.org>
2025-12-17 18:15:58 +0100

    commit5

--- app/src/main/java/org/mathieu/cleanrmapi/data/repositories/LocationRepositoryImpl.kt
+++ app/src/main/java/org/mathieu/cleanrmapi/data/repositories/LocationRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package org.mathieu.cleanrmapi.data.repositories
 
+import kotlinx.serialization.InternalSerializationApi
 import org.mathieu.cleanrmapi.data.remote.CharacterApi
 import org.mathieu.cleanrmapi.data.remote.responses.LocationResponse
 import org.mathieu.cleanrmapi.domain.models.location.Location
@@ -9,10 +10,12 @@ internal class LocationRepositoryImpl(
     private val locationApi: CharacterApi
 ) : LocationRepository {
 
+    @OptIn(InternalSerializationApi::class)
     override suspend fun getLocation(id: Int): Location =
         locationApi.getLocationId(id = id)?.toDomain()
             ?: throw Exception("Location not found")
 }
+@OptIn(InternalSerializationApi::class)
 private fun LocationResponse.toDomain(): Location? {
     return Location(
         id = id,
